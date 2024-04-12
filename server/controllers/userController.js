7061f53dfbbcbedcd636b32ed906ac3175352946
Kelvin Wallace <kw222sw@student.lnu.se>
2024-04-12 16:12:07 +0200

    Now you can signup through postman. Still need to make changes to config in userModel

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,26 @@
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt')
 
-function signup(req, res) {}
+async function signup(req, res) {
+    try {
+    //get the email and password from req body
+    const {email, password} = req.body
+    
+    //hash password
+    const hashedPassword = bcrypt.hashSync(password, 10)
 
-function login(req, res) {}
+    //create a user with the data
+    await User.create({email, password: hashedPassword})
+    //respond
+    res.sendStatus(200)
+    } catch(error) {
+        console.log('BLUP BLUP PULL UP', error)
+        res.sendStatus(400)
+    }
+}
+
+function login(req, res) {
+}
 
 function logout(req, res) {}
 
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema({
  email: {
@@ -8,21 +6,11 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
     lowercase: true,
-    trim: true,
-    validate: {
-      validator: function(v) {
-        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
-      },
-      message: props => `${props.value} is not a valid email address!`
-    }
+    index: true
  },
  password: {
     type: String,
     required: true,
-    select: false,
-    set: function(value) {
-      return bcrypt.hashSync(value, SALT_WORK_FACTOR);
-    }
  }
 });
 
