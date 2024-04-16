6e2e3edff3e6e997ebf1667638906301c426a8b5
Kelvin Wallace <kw222sw@student.lnu.se>
2024-04-16 16:58:51 +0200

    Fixed the app.js and carsStore.js files so they work. The code is now a lot cleaner and works as intended.

--- reactfrontend/src/components/App.js
+++ reactfrontend/src/components/App.js
@@ -1,134 +1,65 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useEffect } from 'react'
 import carsStore from '../stores/carsStore'
+
 
 function App() {
 
-//stores
+
+ const {
+   createCar,
+   updateCar,
+   updateCreateCarField,
+   createdCar,
+   deleteCar,
+   handleUpdateFieldChange,
+   toggleUpdate,
+   carUpdate,
+} = carsStore()
+
 const store = carsStore()
 
-//state
-  const[cars, setCars] = useState(null)
-  const [createCar, setCreateCar] = useState({
-    title: '',
-    body: ''
-  })
-  const [updateCar, setUpdateCar] = useState({
-    _id: null,
-    title: '',
-    body: ''
-  })
-  //use effect
-  useEffect(() => {
-    store.fetchCars()
-  }, [])
-
-  const updateCreateCarField = (e) => {
-    const {name, value} = e.target
-    setCreateCar({
-      ...createCar,
-      [name]: value,
-    })
-  }
-
-  //create car
-  const createdCar = async (e) => {
-    e.preventDefault()
-
-    //create car
-const res = await axios.post('http://localhost:8080/cars', createCar)
-    //update state
-
-    setCars([...cars, res.data.car])
-
-    //clear state
-    setCreateCar({title: '', body: ''})
-  }
-
-  //delete car
-  const deleteCar = async (_id) => {
-    //delete the car
-    await axios.delete(`http://localhost:8080/cars/${_id}`)
-
-    //update state
-    const newCars = [...cars].filter(car => {
-      return car._id !== _id
-    })
-    setCars(newCars)
-  }
-
-  const handleUpdateFieldChange = (e) => {
-    const {value, name} = e.target
-
-    setUpdateCar({
-      ...updateCar,
-      [name]: value
-    })
-  }
-
-  const toggleUpdate = (car) => {
-    //set state on update form
-
-    setUpdateCar({title: car.title, body:car.body, _id: car._id})
-  }
-
-  const carUpdate = async (e) => {
-    e.preventDefault()
-    const {title, body} = updateCar
-    //send the update request
-    const res = await axios.put(`http://localhost:8080/cars/${updateCar._id}`, {title, body})
-    
-    //update state
-    const newUpdatedCar = [...cars]
-    const carIndex = cars.findIndex(car => {
-      return car._id === updateCar._id
-    })
-    newUpdatedCar[carIndex] = res.data.car
-
-    setCars (newUpdatedCar)
-
-    //clear state
-    setUpdateCar({
-      _id: null,
-      title: '',
-      body: ''
-    })
-  }
-
-  return <><div>
-    <h2>Cars: </h2>
-    {store.cars && store.cars.map(car => {
-      return (<div key={car._id}>
-        <h3>{car.title}</h3>
-        <button onClick={() => deleteCar(car._id)}>Delete car</button>
-        <button onClick={() => toggleUpdate(car)}>Update car</button>
-      </div>) 
-      })}
-    </div>
+ //use effect
+ useEffect(() => {
+   store.fetchCars()
+ }, )
 
 
-  {updateCar._id && ( 
-    <div>
-  <h2>Update car</h2>
-  <form onSubmit={carUpdate}>
-    <input onChange={handleUpdateFieldChange} value={updateCar.title} name='title'/>
-    <textarea onChange={handleUpdateFieldChange} value = {updateCar.body} name='body'/>
-    <button type='submit'>Update car</button>
-  </form>
-  </div> 
-  )}
+ return <><div>
+   <h2>Cars: </h2>
+   {store.cars && store.cars.map(car => {
+     return (<div key={car._id}>
+       <h3>{car.title}</h3>
+       <button onClick={() => deleteCar(car._id)}>Delete car</button>
+       <button onClick={() => toggleUpdate(car)}>Update car</button>
+     </div>)
+     })}
+   </div>
 
- 
- 
- {!updateCar._id && ( <div>
-          <h2>Create car:</h2>
-      <form onSubmit={createdCar}>
-        <input onChange={updateCreateCarField} value={createCar.title} name='title' />
-        <textarea  onChange={updateCreateCarField} value={createCar.body} name='body' />
-        <button type='submit'>Create car</button>
-      </form>
-    </div>)}
-    </>
+
+
+
+ {updateCar._id && (
+   <div>
+ <h2>Update car</h2>
+ <form onSubmit={carUpdate}>
+   <input onChange={handleUpdateFieldChange} value={updateCar.title} name='title'/>
+   <textarea onChange={handleUpdateFieldChange} value = {updateCar.body} name='body'/>
+   <button type='submit'>Update car</button>
+ </form>
+ </div>
+ )}
+
+
+{!updateCar._id && ( <div>
+         <h2>Create car:</h2>
+     <form onSubmit={createdCar}>
+       <input onChange={updateCreateCarField} value={createCar.title} name='title' />
+       <textarea  onChange={updateCreateCarField} value={createCar.body} name='body' />
+       <button type='submit'>Create car</button>
+     </form>
+   </div>)}
+   </>
 }
+
 
 export default App;
--- reactfrontend/src/stores/carsStore.js
+++ reactfrontend/src/stores/carsStore.js
@@ -1,17 +1,68 @@
+// carsStore.js
+
 import { create } from 'zustand'
 import axios from 'axios'
 
-const carsStore = create((set) => ({
 
-  cars: null,
-
-  fetchCars: async () => {
-    console.log('blup blup it works!!! ')
-    //fetch notes
-  const res = await axios.get('http://localhost:8080/cars') 
-  //set it on state
-  set({cars: res.data.cars})
-  }
+// Define your store
+const CarsStore = create((set) => ({
+cars: [],
+createCar: {
+   title: '',
+   body: '',
+},
+updateCar: {
+   _id: null,
+   title: '',
+   body: '',
+},
+fetchCars: async () => {
+   const res = await axios.get('http://localhost:8080/cars')
+   set({ cars: res.data.cars })
+},
+updateCreateCarField: (e) => {
+   const { name, value } = e.target;
+   set((state) => ({
+     createCar: { ...state.createCar, [name]: value },
+   }))
+},
+createdCar: async (e) => {
+   e.preventDefault()
+   const res = await axios.post('http://localhost:8080/cars', CarsStore.getState().createCar)
+   set((state) => ({
+     cars: [...state.cars, res.data.car],
+     createCar: { title: '', body: '' },
+   }))
+},
+deleteCar: async (_id) => {
+   await axios.delete(`http://localhost:8080/cars/${_id}`)
+   set((state) => ({
+     cars: state.cars.filter((car) => car._id !== _id),
+   }))
+},
+handleUpdateFieldChange: (e) => {
+   const { value, name } = e.target
+   set((state) => ({
+     updateCar: { ...state.updateCar, [name]: value },
+   }))
+},
+toggleUpdate: (car) => {
+   set({
+     updateCar: { title: car.title, body: car.body, _id: car._id },
+   })
+},
+carUpdate: async (e) => {
+   e.preventDefault()
+   const { title, body } = CarsStore.getState().updateCar;
+   const res = await axios.put(`http://localhost:8080/cars/${CarsStore.getState().updateCar._id}`, { title, body })
+   set((state) => {
+     const newUpdatedCar = [...state.cars]
+     const carIndex = state.cars.findIndex((car) => car._id === CarsStore.getState().updateCar._id)
+     newUpdatedCar[carIndex] = res.data.car
+     return { cars: newUpdatedCar, updateCar: { _id: null, title: '', body: '' } }
+   })
+},
 }))
 
-export default carsStore
+
+export default CarsStore;
