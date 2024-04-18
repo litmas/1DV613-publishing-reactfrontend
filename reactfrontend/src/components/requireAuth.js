c96de9c9ca1f45c0e5aa4050bef28ed811db6a4e
Kelvin Wallace <kw222sw@student.lnu.se>
2024-04-18 14:23:27 +0200

    automatically redirect to home page after login

--- reactfrontend/src/components/requireAuth.js
+++ reactfrontend/src/components/requireAuth.js
@@ -1,22 +1,27 @@
 import { useEffect } from "react"
+import { Navigate } from 'react-router-dom'
 import authStore from "../stores/authStore"
 
 export default function requireAuth(props) {
 
     const store = authStore()
 
-    
+     
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
         if(store.loggedIn === null) {
-            store.authCheck()
+            store.checkAuth()
         }
         // same problem as in carsPage.js
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
-    if (!store.loggedIn) {
-        return <div> Please login </div>
+    if (store.loggedIn === null) {
+        return <div> Loading </div>
+    }
+
+    if (store.loggedIn === false) {
+        return <Navigate to='/login'/>
     }
 
     return (
